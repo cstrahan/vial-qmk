a466af43b5dbab60578023056a6b5237f33cc2e4
Charles Strahan <cstrahan@fullstory.com>
2023-08-09 19:10:54 -0500

    kimiko: enable more

--- keyboards/keycapsss/kimiko/keymaps/vial/rules.mk
+++ keyboards/keycapsss/kimiko/keymaps/vial/rules.mk
@@ -1,9 +1,18 @@
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+OLED_ENABLE = yes
+#OLED_DRIVER = SSD1306
 
 VIA_ENABLE = yes		   # Enable via support
 VIAL_ENABLE = yes		   # Enable vial support
 
 LTO_ENABLE = yes
 
-QMK_SETTINGS = no
+QMK_SETTINGS = yes
+ENCODER_MAP_ENABLE = yes
+
+MOUSEKEY_ENABLE = yes      # Mouse keys
+EXTRAKEY_ENABLE = yes      # Audio control and System control
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+AUDIO_ENABLE = no          # Audio output
+
 ENCODER_MAP_ENABLE = yes
